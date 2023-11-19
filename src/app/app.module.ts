789a7f13b7a6bed965afae57c136340511665c61
k1nteiro <k5.k1nteiro@gmail.com>
2023-11-19 14:11:43 +0100

    Añadir botones de socials y aplicar nuevos estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomePresentationComponent } from './home/homePresentation/homePresentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    ContactComponent
+    ContactComponent,
+    HomePresentationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/homePresentation/homePresentation.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-presentation',
+  templateUrl: './homePresentation.component.html',
+  styleUrls: ['./homePresentation.component.scss']
+})
+export class HomePresentationComponent {
+
+
+  goToLinkedIn() : void {
+    window.open("https://www.linkedin.com/in/squinteiro/", "_blank");
+  }
+
+  goToInstagram() : void {
+    window.open("https://www.instagram.com/k1nteiro/", "_blank");
+  }
+
+  goToFacebook() : void {
+    window.open("https://www.facebook.com/k5.k1nteiro", "_blank");
+  }
+
+  goToTwitter() : void {
+    window.open("https://twitter.com/k1nteiro", "_blank");
+  }
+}
